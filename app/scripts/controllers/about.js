f3641e2b2f258d188c859caa75aef19d37c52214
anilchoudhary <masteranilchoudhary@gmail.com>
2020-04-02 22:25:48 +0530

    initial version of fdas portal

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fdasportalApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fdasportalApp
+ */
+angular.module('fdasportalApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fdasportalApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fdasportalApp
+ */
+angular.module('fdasportalApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
